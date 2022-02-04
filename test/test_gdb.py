58df2101d21daf2caf2de115d4633bc77b94390c
Mauri Mustonen <mauri_mustonen@hotmail.com>
2022-02-04 22:33:29 +0200

    try

--- test/test_gdb.py
+++ test/test_gdb.py
@@ -47,7 +47,7 @@ def test_change_skipcalls_value():
 
 
 def test_savecfg():
-    execute_gdb_commands(['set confirm off', 'file ls', 'starti', 'savecfg'])
+    execute_gdb_commands(['set confirm off', 'set breakpoint pending on', 'file ls', 'b _start', 'run', 'savecfg'])
     assert os.path.isfile('_start.pdf')
     # assert 'Saved CFG to a file _start.pdf' in result.stdout, result.stdout
 
