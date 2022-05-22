75e0440f06ebfdde85619bd4008a2077509bd92d
Mauri Mustonen <mauri.mustonen@hotmail.com>
2022-05-22 18:23:54 +0300

    install python distutils

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -6,7 +6,7 @@ set -x
 PYTHON=${PYTHON:-python3}
 
 sudo apt-get update
-sudo apt-get install $PYTHON gdb graphviz -y
+sudo apt-get install $PYTHON gdb graphviz -y ${PYTHON}-distutils
 # Install graphviz globally because of GDB using global python
 # installation and not active pipenv.
 pip3 install graphviz
