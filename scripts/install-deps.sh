9c8b520e0b4dc14ca34e97e90165914684d4c187
Mauri Mustonen <mauri.mustonen@hotmail.com>
2022-05-22 16:06:46 +0300

    install specific pipenv version for Ubuntu 18.04

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -10,5 +10,3 @@ sudo apt-get install $PYTHON gdb graphviz -y
 # Install graphviz globally because of GDB using global python
 # installation and not active pipenv.
 pip3 install graphviz
-$PYTHON -m pip install pipenv
-pipenv install --dev
